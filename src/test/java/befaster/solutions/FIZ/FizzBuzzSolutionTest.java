b723ee068e5db2248694d182b7eb44762486fbc8
sbx_user1060 <sbx_user1060@ip-10-129-2-238.eu-west-2.compute.internal>
2018-10-08 07:09:31 +0000

    Mon Oct 08 07:09:31 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -28,15 +28,24 @@ public class FizzBuzzSolutionTest {
         assertThat(solution.fizzBuzz(number), equalTo(result));
     }
 
-    /*
-        - A number is considered to be "deluxe" if it is greater than 10 and all the digits are identical
-        - A number can be "fizz", "buzz" and "deluxe" at the same time. If this happens then write "fizz buzz deluxe"
-            muliple of three, contains 5 and all digit are 5 -> 555
-            555 is odd so it should be fake deluxe
-     */
     @Test
     @Parameters({ "555, fizz buzz fake deluxe"})
     public void fizzBuzzDeluxe(Integer number, String result) throws Exception {
+        assertThat(solution.fizzBuzz(number), equalTo(result));
+    }
+
+    /**
+        - The old rules for "deluxe" do not apply.
+        - A number is "fizz deluxe" if it is divisible by 3 AND it has a 3 in it
+        - A number is "buzz deluxe" if it is divisible by 5 AND it has a 5 in it
+        - We should still say "fake deluxe" if the "deluxe" number is odd
+        - A number can be "fizz", "buzz" and "deluxe" at the same time. If this happens then write "fizz buzz deluxe"
+        - All the previous rules for "fizz" and "buzz" are still valid
+     */
+
+    @Test
+    @Parameters({ "555, fizz buzz fake deluxe"})
+    public void fizzBuzzNewDeluxe(Integer number, String result) throws Exception {
         assertThat(solution.fizzBuzz(number), equalTo(result));
     }
 
