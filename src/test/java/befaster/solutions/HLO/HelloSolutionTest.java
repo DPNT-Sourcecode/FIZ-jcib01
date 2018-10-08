5c41e47f179745697a50c9bc8668b9b06234c442
sbx_user1060 <sbx_user1060@ip-10-129-2-238.eu-west-2.compute.internal>
2018-10-08 06:06:26 +0000

    Mon Oct 08 06:06:26 UTC 2018

---
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -0,0 +1,14 @@
+package befaster.solutions.HLO;
+
+import org.junit.Test;
+
+/**
+ * Created by sujith on 08/10/2018.
+ */
+public class HelloSolutionTest {
+    @Test
+    public void hello() throws Exception {
+
+    }
+
+}
